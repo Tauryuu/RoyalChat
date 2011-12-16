bddd1894184d52aa96202d2dc4016411565ab7b3
Kyle Clemens <jkc.clemens@gmail.com>
2011-12-15 20:37:18 -0500

    Version 0.0.35

--- src/tk/royalcraf/royalchat/RoyalChat.java
+++ src/tk/royalcraf/royalchat/RoyalChat.java
@@ -31,7 +31,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class RoyalChat extends JavaPlugin {
 
-	public String version = "0.0.3";
+	public String version = "0.0.35";
 
 	Logger log = Logger.getLogger("Minecraft");
 
