4a23c64ae575d9bf82877195abdb94338ddbf60f
Kyle Clemens <jkc.clemens@gmail.com>
2012-02-02 22:11:17 -0500

    Updated: Towny path

--- src/tk/royaldev/royalchat/RoyalChatCommands.java
+++ src/tk/royaldev/royalchat/RoyalChatCommands.java
@@ -7,6 +7,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import tk.royaldev.royalchat.utils.TownyUtils;
 
 public class RoyalChatCommands implements CommandExecutor {
 
